bcdb180902de713003a53c9c6c965f6f47c770af
erihidayat06 <erihidayat549@gmail.com>
2023-10-17 13:07:57 +0700

    Mengubah Icon

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -9,7 +9,7 @@
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
     {{-- Logo Title --}}
-    <link href="/assets/img/logo-perpus.png" rel="icon">
+    <link href="/assets/img/icon-perpus.png" rel="icon">
 
     {{-- icon bootstrap --}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
