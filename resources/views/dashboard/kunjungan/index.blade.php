d4e1e4257c01453fb625bf847925fcfc2d1e64ff
erihidayat06 <erihidayat549@gmail.com>
2023-10-18 07:06:46 +0700

    Menambahkan Total Pengunjung

--- resources/views/dashboard/kunjungan/index.blade.php
+++ resources/views/dashboard/kunjungan/index.blade.php
@@ -62,6 +62,27 @@
                             </form>
                         </div>
 
+                        <div class="col-xxl-4 col-xl-12">
+                            <div class="card info-card sales-card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Pengujung <span>| Total</span></h5>
+
+                                    <div class="d-flex align-items-center">
+                                        <div
+                                            class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                            <i class="bi bi-people"></i>
+                                        </div>
+                                        <div class="ps-3">
+                                            <h6>{{ $kunjungans->sum('jumlah') }}</h6>
+
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+
+                        </div><!-- End Customers Card -->
+
                         <div class="row">
                             <div class="col-xxl-3 col-xl-12">
                                 <div class="card info-card customers-card">
